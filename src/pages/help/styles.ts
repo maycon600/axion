7d734e63b9523ab23d6a290ea7699d275946849b
VictorOgawa <victor.ogawa@gmail.com>
2023-12-05 14:28:53 -0400

    Add missing pages

--- src/pages/help/styles.ts
+++ src/pages/help/styles.ts
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
   background-color: ${({ theme }) => theme.color.gray_10};
   margin: 0.37rem 0 1rem 0.4rem;
   border-radius: 25px 0 0 25px;
@@ -8,4 +10,30 @@ export const Content = styled.div`
   position: relative;
   width: calc(100% - 17rem);
   left: calc(100% - 17rem);
+  align-items: center;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  background-color: white;
+  width: 90%;
+  height: 100%;
+  margin: 2%;
+  border-radius: 10px;
+  padding: 2%;
+`;
+
+export const Cards = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  border-radius: 10px;
+  border: 1px solid black;
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.25);
+  background-color: white;
+  width: 25%;
+  margin: 2%;
+  padding: 1%;
 `;
